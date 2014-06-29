1005ef0e01dfbc45cfc6ac498b5e1dbdf8520d4b
Joonas Aalto <joaalto@iki.fi>
2014-06-29 23:17:21 +0300

    Include spyscope and pretty stacktraces.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,12 +1,18 @@
 {:user {
         :plugins [[lein-exec "0.3.1"]]
-        :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                       [im.chit/vinyasa "0.1.8"]]
+        :dependencies [;[org.clojure/tools.namespace "0.2.4"]
+                       [im.chit/vinyasa "0.1.8"]
+                       [spyscope "0.1.4"]
+                       [io.aviso/pretty "0.1.12"]]
         :injections [(require 'vinyasa.inject)
-                     (require '[clojure.tools.namespace.repl :refer [refresh]])
+                     (require 'spyscope.core)
+                     ;(require '[clojure.tools.namespace.repl :refer [refresh]])
                      (use '[clojure.stacktrace])
                      (vinyasa.inject/inject 'clojure.core '>
                        '[[clojure.repl doc source]
                          [clojure.pprint pprint pp]])]
-        }}
+        :repl-options {
+          :nrepl-middleware [io.aviso.nrepl/pretty-middleware]
+        }
+}}
 
