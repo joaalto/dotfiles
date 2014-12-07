e50226aa81e90d96d838dbac6a898ace028dbd95
Joonas Aalto <joaalto@iki.fi>
2014-12-07 15:05:04 +0200

    Upgrade spyscope.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
         :plugins [[lein-exec "0.3.1"]]
         :dependencies [;[org.clojure/tools.namespace "0.2.4"]
                        [im.chit/vinyasa "0.1.8"]
-                       [spyscope "0.1.4"]
+                       [spyscope "0.1.5"]
                        [io.aviso/pretty "0.1.12"]]
         :injections [(require 'vinyasa.inject)
                      (require 'spyscope.core)
